4993771d538e91333f87a020fe9e6f6824092300
fab83200 <58056424+fab83200@users.noreply.github.com>
2022-10-11 14:18:54 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,20 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import pandas as pd
 
-"""
-# Welcome to Streamlit!
+st.title("Fabrice Dashboard 1.0")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+st.markdown('This is my lit Dashboard')
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+evening_options = ['Rim', 'Baby Making', 'Private Garden']
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+"Selectbox from a list"
 
+selected_choice = st.selectbox(label="What do you want to do tonight?",
+                                options=evening_options)
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Nombre de points dans la spirale", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if selected_choice == 'Rim':
+  "I need to take a shower first"
+elif selected_choice == 'Baby Making':
+  "You won't be able to walk for an hour"
+else:
+  "I go get the KY, prepare yourself for a strong pounding"
