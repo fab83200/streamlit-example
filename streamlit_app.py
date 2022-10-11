83a1721177e7dd02a10b92fa3ffcb16cf8548d30
fab83200 <58056424+fab83200@users.noreply.github.com>
2022-10-11 14:41:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-'''
-##########################
-# Script for a SelectBox #
-##########################
-# "Text Intro: Select from a list"
-features_available = ['Option1', 'Option2', 'Option3']
-selected_choice = st.selectbox(label="What Feature doy ou want to analyze?",
-                                options=features_available)
-if selected_choice == 'Option1':
-  "Conclusion1"
-elif selected_choice == 'Option2':
-  "Conclusion2"
-else:
-  "Conclusion3"
-'''
-
-
 #Will create a markdown:
 #  '#' introduce a chapter mark.
 #  '_' introduce an italic string.
@@ -28,7 +11,7 @@ Here is my first _tryout_.
 ''')
 
 
-col1, buff, col2 = st.columns([1, .5, 3])
+col1, buff, col2 = st.columns([1, 1, 2])
 
 def lbs_to_kg():
   st.session_state.kg = st.session_state.lbs / 2.2046                 # calls `lbs` and stores it under `kg` after computation
@@ -37,14 +20,11 @@ def kg_to_lbs():
   st.session_state.lbs = st.session_state.kg * 2.2046                 # calls `kg` and stores it under `lbs` after computation
 
 with col1:
-  pounds = st.number_input("Pounds", key='lbs', on_change= lbs_to_kg)
+  pounds = st.number_input("Pounds",                                  # Name of the number_input
+                           key='lbs',                                 # Name of the variable for the data
+                           on_change= lbs_to_kg)                      # Name of the function to use `on_change`, it would be `on_click` for one-off widgets
 
 with col2:
-  kilogram = st.number_input("Kilos", key='kg', on_change= kg_to_lbs)
-
-'''
-  on_change for input widgets like st.slider or st.number_input
-  on_click for one-off widgets like st.button or st.form_submit_button
-'''
-
-st.write(app_train)
+  kilogram = st.number_input("Kilos",                                 # Name of the number_input
+                             key='kg',                                # Name of the variable for the data
+                             on_change= kg_to_lbs)                    # Name of the function to use `on_change`, it would be `on_click` for one-off widgets
