a3cd9103c988094409b8b7d952c59beaf76326ec
fab83200 <58056424+fab83200@users.noreply.github.com>
2022-10-11 15:04:12 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,23 +5,25 @@ col1, buffer_column, col2 = st.columns([2, .5, 2])                    # sets up 
 
 features_available = ['Option1', 'Option2', 'Option3']
 
-next = st.button("Next Option")
+next = st.button("Next Option")                                       # creates an on_click button with label "Next..." and named `next`
 
-if next:
-  if st.session_state.my_option == 'Option1':
-    st.session_state.my_option = 'Option2'
-  elif st.session_state.my_option == 'Option2':
-    st.session_state.my_option = 'Option3'
+if next:                                                              # creates the rotary effect on the radio_button
+  if st.session_state.radio_button == 'Option1':
+    st.session_state.radio_button = 'Option2'
+  elif st.session_state.radio_button == 'Option2':
+    st.session_state.radio_button = 'Option3'
   else:
-    st.session_state.my_option = 'Option1'
+    st.session_state.radio_button = 'Option1'
 
-option = col1.radio("Pick an option:", features_available, key="my_option")        # display a radio button widget
+radio_button = col1.radio("Pick an option:",                                # creates a Radio_Buttons widget labeled "Pick..." and named `radio_button`
+                    features_available,                               # creates as buttons as features declared
+                    key="radio_button")                               # names the radio button widget `radio_button`
 
-if option == 'Option1':
+if radio_button == 'Option1':
   col2.write("You picked `Option1` Conclusion1")
-if option == 'Option2':
+if radio_button == 'Option2':
   col2.write("You picked `Option2` Conclusion2")
-if option == 'Option3':
+if radio_button == 'Option3':
   col2.write("You picked `Option3` Conclusion3")
 
-st.session_state
+#st.session_state
