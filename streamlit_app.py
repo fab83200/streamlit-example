b21f5706274365983eb7dd2c951cf05689385498
fab83200 <58056424+fab83200@users.noreply.github.com>
2022-10-11 14:24:50 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,13 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Fabrice Dashboard 1.0")
+'''
+features_available = ['Rim', 'Baby Making', 'Private Garden']
 
-st.markdown('This is my lit Dashboard')
+"Select from a list"
 
-evening_options = ['Rim', 'Baby Making', 'Private Garden']
-
-"Selectbox from a list"
-
-selected_choice = st.selectbox(label="What do you want to do tonight?",
-                                options=evening_options)
+selected_choice = st.selectbox(label="What Feature doy ou want to analyze?",
+                                options=features_available)
 
 if selected_choice == 'Rim':
   "I need to take a shower first"
@@ -18,3 +15,4 @@ elif selected_choice == 'Baby Making':
   "You won't be able to walk for an hour"
 else:
   "I go get the KY, prepare yourself for a strong pounding"
+'''
