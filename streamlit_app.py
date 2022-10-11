0621640f4d1c7cba168f24ace0e16a6a99a2427e
fab83200 <58056424+fab83200@users.noreply.github.com>
2022-10-11 15:13:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,7 @@ import streamlit as st
 import pandas as pd
 
 col1, buffer_column, col2 = st.columns([2, .5, 2])                    # sets up columns of standard width where sum is the total width
-
-features_available = ['Option1', 'Option2', 'Option3']
-
+features_available = ['Option1', 'Option2', 'Option3']                # sets the available features
 next = st.button("Next Option")                                       # creates an on_click button with label "Next..." and named `next`
 
 if next:                                                              # creates the rotary effect on the radio_button
@@ -15,11 +13,11 @@ if next:                                                              # creates 
   else:
     st.session_state.radio_button = 'Option1'
 
-radio_button = col1.radio("Pick an option:",                                # creates a Radio_Buttons widget labeled "Pick..." and named `radio_button`
+radio_button = col1.radio("Pick an option:",                          # creates a Radio_Buttons widget labeled "Pick..." and named `radio_button`
                     features_available,                               # creates as buttons as features declared
                     key="radio_button")                               # names the radio button widget `radio_button`
 
-if radio_button == 'Option1':
+if radio_button == 'Option1':                                         # conditions the outcome of picking up a radio_button
   col2.write("You picked `Option1` Conclusion1")
 if radio_button == 'Option2':
   col2.write("You picked `Option2` Conclusion2")
