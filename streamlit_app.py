43d449680acc84a81c17a31bfe3247fe8fbfb910
fab83200 <58056424+fab83200@users.noreply.github.com>
2022-10-11 16:04:27 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,46 @@
+#!pip install -q lime
+#!pip install -q streamlit
+
+#COLAB
+from google.colab import drive                                                  # NEEDED
+drive.mount('/content/gdrive');      
+
 import streamlit as st
+import pandas as pd
+import numpy as np
+import random
 
-st.session_state
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
-if 'my_boolean' not in st.session_state:                                        # if the variable doesn't exist, it stores it
-  st.session_state['my_boolean'] = True
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression
 
-if 'my_counter' not in st.session_state:                                        # if the variable doesn't exist, it stores it
-  st.session_state['my_counter'] = 5
+from sklearn.metrics import accuracy_score
+from lime import lime_tabular
+import streamlit.components.v1 as components
 
-button = st.button("Update Values")
 
-if button:
-  "Before pressing Button:"
-  st.write("  - The value of my counter is: ", st.session_state.my_counter)
-  st.write("  - The value of my boolean is: ", st.session_state.my_boolean)
+title_text = 'LIME Explainer Dashboard for credit score'
 
-  st.session_state.my_counter += 1
-  st.session_state.my_boolean = not st.session_state.my_boolean
+st.markdown(f"<h2 style='text-align: center;'><b>{title_text}</b></h2>", unsafe_allow_html=True)
+st.text("")
 
-  "After pressing Button:"
-  st.write("  - The value of my counter is: ", st.session_state.my_counter)
-  st.write("  - The value of my boolean is: ", st.session_state.my_boolean)
 
-#for key in st.session_state.keys():
-#  del st.session_state[key]
+# This is the main train table, with TARGET
+application_train_url = "/content/gdrive/MyDrive/Colab_Notebooks/Project7/application_train.csv"
+app_train = pd.read_csv(application_train_url, sep=",")
+for tr in app_train.describe(include='object').columns:
+    app_train[tr]=app_train[tr].fillna((app_train[tr].mode()))
+for ci in app_train.describe().columns:
+    app_train[ci]=app_train[ci].fillna((app_train[ci].median()))
 
-for items in st.session_state.items():                                          # to display all pairs (or .keys() or .values() for individual display)
-  st.write(items)
 
+# This is the main test table, without TARGET
+application_test_url = "/content/gdrive/MyDrive/Colab_Notebooks/Project7/application_test.csv"
+app_test = pd.read_csv(application_test_url, sep=",")
+for te in app_test.describe(include='object').columns:
+    app_test[te]=app_test[te].fillna((app_test[te].mode()))
+for tt in app_test.describe().columns:
+    app_test[tt]=app_test[tt].fillna((app_test[tt].median()))
