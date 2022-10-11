c56e75d7d2ffb3506cf01bf16989ee6e4685c8f9
fab83200 <58056424+fab83200@users.noreply.github.com>
2022-10-11 16:33:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import random
 
-#from sklearn.preprocessing import LabelEncoder
+import sklearn.preprocessing.LabelEncoder
 #from sklearn.preprocessing import StandardScaler
 #from sklearn.model_selection import train_test_split
 
