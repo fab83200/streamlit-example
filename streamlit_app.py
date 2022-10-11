a5ab4d15b44cc9f1f5ebe86f6f1a1d9accdfde39
fab83200 <58056424+fab83200@users.noreply.github.com>
2022-10-11 14:27:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,15 @@
 import streamlit as st
 import pandas as pd
 
-features_available = ['Rim', 'Baby Making', 'Private Garden']
 
-"Select from a list"
-
+# "Text Intro: Select from a list"
+features_available = ['Option1', 'Option2', 'Option3']
 selected_choice = st.selectbox(label="What Feature doy ou want to analyze?",
                                 options=features_available)
 
-if selected_choice == 'Rim':
-  "I need to take a shower first"
-elif selected_choice == 'Baby Making':
-  "You won't be able to walk for an hour"
+if selected_choice == 'Option1':
+  "Conclusion1"
+elif selected_choice == 'Option2':
+  "Conclusion2"
 else:
-  "I go get the KY, prepare yourself for a strong pounding"
+  "Conclusion3"
